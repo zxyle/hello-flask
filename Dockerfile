2edffc2c5b049ba31c8c3ecedbdc1b0bbc666440
zxyle <zxyful@gmail.com>
2019-07-22 11:11:32 +0800

    add docker support

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+FROM python:3.7.4
+MAINTAINER Zheng zxyful@gmail.com
+
+LABEL Version="1.0" \
+      LastModifiedTime="2019-07-22"
+
+ADD . /app/
+
+WORKDIR /app/
+
+COPY ./pip.conf /etc/pip.conf
+
+# 时区设置
+ENV TZ=Asia/Shanghai
+
+RUN pip install --no-cache-dir --upgrade pip \
+     && pip install --no-cache-dir -r requirements.txt
+
+RUN chmod a+x ./entrypoint.sh
+
+EXPOSE 5000
+
+ENTRYPOINT ["./entrypoint.sh"]
---
+++ gunicorn.conf.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Author: Zheng <zhengxiang@upg.cn>
+"""gunicorn 配置文件"""
+# 使用以下命令执行
+# gunicorn -c gunicorn.conf.py app:app
+# 详细配置参数请参考以下url
+# http://docs.gunicorn.org/en/latest/settings.html
+
+import multiprocessing
+
+bind = "0.0.0.0:5000"
+workers = multiprocessing.cpu_count() * 2 + 1
+
+# Daemonize the Gunicorn process.
+daemon = False
+
+# restart workers when code changes.
+reload = True
+
+# process id
+pidfile = "/tmp/spider_server.pid"
+
+# logfile
+errorlog = "/tmp/spider_server.log"
+
+# the Access log file to write to.
+accesslog = "/tmp/spider_server.log"
