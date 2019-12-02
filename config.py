befc27c0ae19e3f43be2249e85146edee6dd01b9
zxyle <zxyful@gmail.com>
2019-12-02 16:36:03 +0800

    配置信息增加注释

--- config.py
+++ config.py
@@ -5,8 +5,7 @@
 # Desc: 
 
 
+# 哲象OSS配置信息
 access_key_id = "LTAIEG7I3qE4IYkl"
 access_key_secret = "qyiGHHmhGbjF0apsnzqQr9pAM43hkr"
 endpoint = "oss-cn-hangzhou.aliyuncs.com"
-bucket_name = "zx-scenic"
-bucket_domain = f"https://{bucket_name}.{endpoint}"
