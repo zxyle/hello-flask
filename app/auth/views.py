4eefb874d61c646c38cb275e71614465071470a3
zxyle <zxyful@gmail.com>
2019-12-05 13:55:59 +0800

    用户鉴权 使用加盐方式

--- app/auth/views.py
+++ app/auth/views.py
@@ -5,6 +5,7 @@
 # Desc: 
 
 from flask import request
+from werkzeug.security import check_password_hash, generate_password_hash
 
 from app import db
 from . import auth_blue
@@ -14,9 +15,14 @@ from ..models import User
 @auth_blue.route('/login', methods=['GET', 'POST'])
 def login():
     username = request.form.get("username")
+    user = User.query.filter_by(username=username).first()
+    if not user:
+        return "未注册"
+    password_hash = user.password
     password = request.form.get("password")
+    status = check_password_hash(password_hash, password)
 
-    return "{}-{}".format(username, password)
+    return "Login {}".format(status)
 
 
 @auth_blue.route('/logout', methods=['GET', 'POST'])
@@ -37,7 +43,8 @@ def register():
 
     username = form.get("username")
     password = form.get("password")
-    user = User(username=username, password=password)
+    password_hash = generate_password_hash(password, method="pbkdf2:sha256", salt_length=8)
+    user = User(username=username, password=password_hash)
     db.session.add(user)
     db.session.commit()
     return "Welcome {}!".format(username)
@@ -45,4 +52,20 @@ def register():
 
 @auth_blue.route('/change-password', methods=['GET', 'POST'])
 def change_password():
-    pass
+    username = request.form.get("username")
+    old_password = request.form.get("old_password")
+    new_password = request.form.get("new_password")
+    user = User.query.filter_by(username=username).first()
+    if not user:
+        return "未注册"
+    password_hash = user.password
+    status = check_password_hash(password_hash, old_password)
+    if not status:
+        return "旧密码错误"
+
+    password_hash = generate_password_hash(new_password, method="pbkdf2:sha256", salt_length=8)
+    user.password = password_hash
+    db.session.add(user)
+    db.session.commit()
+
+    return "change password success."
