581850e3f6057a35001055559ac806cd89d532d9
zxyle <zxyful@gmail.com>
2020-01-18 14:02:29 +0800

    upload view function

--- app/main/views.py
+++ app/main/views.py
@@ -5,8 +5,10 @@
 # Desc:
 
 import logging
+import os
 import socket
 
+from flask import request, render_template, jsonify
 from redis import Redis
 
 from . import main_blue
@@ -14,6 +16,8 @@ from . import main_blue
 redis = Redis(host="redis")
 
 logger = logging.getLogger("app.access")
+
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', "xlsx", "xls", "log"}
 
 
 @main_blue.route('/')
@@ -32,3 +36,39 @@ def hello():
     redis.incr('hits')
     return 'Hello Container World! I have been seen %s times and my hostname is %s.\n' % (
         redis.get('hits'), socket.gethostname())
+
+
+@main_blue.route('/get_version')
+def get_version():
+    latest_version = 1
+    return jsonify({"version": latest_version})
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@main_blue.route('/upload', methods=['POST'])
+def transfer():
+    if request.method != 'POST':
+        return render_template("transfer.html")
+
+    if 'file' not in request.files:
+        return {"msg": "No file part."}
+
+    file = request.files['file']
+    # if user does not select file, browser also
+    # submit an empty part without filename
+    if file.filename == '':
+        return {"msg": "No selected file."}
+
+    if file and allowed_file(file.filename):
+        print(file.filename)
+        # filename = secure_filename(file.filename)
+        filename = file.filename
+        file.save(os.path.join("/uploads", os.path.join(".", filename)))
+
+        return {"status": 1}
+
+    return {"msg": "file error."}
---
+++ tests/test_upload.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Author: Zheng <zxyful@gmail.com>
+# Date: 2020-01-18
+# Desc:
+
+
+import requests
+
+filename = "./专利信息确认表.xlsx"
+url = "http://127.0.0.1:5000/upload"
+r = requests.post(url, files={'file': open(filename, 'rb')})
+print(r.text)
